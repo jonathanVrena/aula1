9c96f8caefba03db97e60ad3d1681c437b371526
jonathanVrena <00001126938087sp@al.educacao.sp.gov.br>
2025-05-12 08:50:13 -0300

    Add files via upload

---
+++ desafio5.py
@@ -0,0 +1,3 @@
+valor = float(input("valor da refeição: R$ "))
+total = valor * 1.10
+print("Total com taxa: ", total)
---
+++ desafio7.py
@@ -0,0 +1,4 @@
+valor = float(input("Valor total: "))
+parcelas = int(input("Número de parcelas: "))
+valor parcelas = valor/parcelas 
+print("Cada parcela: R$ ", valor parcela)
